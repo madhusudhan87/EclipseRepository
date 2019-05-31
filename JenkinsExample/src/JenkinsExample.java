d0adb8b1d19afe4d6a1f402d43c78a6e8d32e8e3
vikas <abc.abcd@gmail.com>
2019-05-31 12:16:07 -0700

    Added arthematic step.

--- JenkinsExample/src/JenkinsExample.java
+++ JenkinsExample/src/JenkinsExample.java
@@ -1,7 +1,13 @@
 
 public class JenkinsExample {
 
-	//public static void main(String[] args) {
+	public static void main(String[] args) {
+	
+		int a=10;
+		int b=0;
+		System.out.println(a/b);
+		
+		
 		System.out.println("this is from eclipse ");
 		System.out.println("added  line ");
 	}
